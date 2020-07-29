4d439da54593b8ff36f95001ecc62ec4000aad06
XiaoPan <2489964425@qq.com>
2020-07-29 15:58:50 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    hi
+    <XiaoPan/>
+  </div>
+  
+</template>
+
+<script>
+import XiaoPan from './components/XiaoPan.vue'
+
+export default {
+  name: 'App',
+  components: {
+    XiaoPan
+  }
+}
+</script>
