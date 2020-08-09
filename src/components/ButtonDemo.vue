feb654d8c8c18c8300df4ba38937f2be12cb7814
XiaoPan <2489964425@qq.com>
2020-08-09 22:05:13 +0800

    Button 组件创建

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
     button
+    <Button>nihao</Button>
   </div>
 </template>
+<script lang="ts">
+import Button from '../lib/Button.vue'
+export default {
+  components: { Button }
+}
+</script>
