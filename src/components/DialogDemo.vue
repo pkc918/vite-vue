8178cf880493515e6791edd844bac1fe7d7d6ee7
XiaoPan <2489964425@qq.com>
2020-08-27 20:49:11 +0800

    Dialog 结构

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    dialog
-  </div>
+<div>
+  <div>Dialog 示例</div>
+  <h3>实列1</h3>
+  <Dialog></Dialog>
+</div>
 </template>
+
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue";
+export default {
+  components: {
+    Dialog,
+  },
+};
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <div class="gulu-dialog-overlay"></div>
+  <div class="gulu-dialog-warpper">
+    <div class="dialog">
+      <header>标题</header>
+      <main>
+        <p>第一行</p>
+        <p>第二行</p>
+      </main>
+      <footer>
+        <Button>OK</Button>
+        <Button>Cancel</Button>
+      </footer>
+    </div>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import Button from "./Button.vue";
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
