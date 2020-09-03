995c0c7f26cde2417187e1734e5a0179112e0580
XiaoPan <2489964425@qq.com>
2020-09-03 15:38:36 +0800

    完成 Tabs 组件

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="gulu-tabs">
   <div class="gulu-tabs-nav" ref="container">
-    <div class="gulu-tabs-nav-item" :class="{selected: t===selected}" v-for="(t,index) in titles" :key="index" :ref=" el => { if(el) navItems[index] = el }" @click="select(t)">{{t}}
+    <div class="gulu-tabs-nav-item" :class="{selected: t===selected}" v-for="(t,index) in titles" :key="index" :ref=" el => { if(t===selected) selectedItem = el }" @click="select(t)">{{t}}
     </div>
     <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
   </div>
@@ -26,24 +26,20 @@ export default {
     },
   },
   setup(props, context) {
-    const navItems = ref < HTMLDivElement[] > ([]);
+    const selectedItem = ref < HTMLDivElement > (null);
     const indicator = ref < HTMLDivElement > (null);
     const container = ref < HTMLDivElement > (null);
     const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter((div) =>
-        div.classList.contains("selected")
-      )[0];
       const {
         width
-      } = result.getBoundingClientRect();
+      } = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + "px";
       const {
         left: navLeft
       } = container.value.getBoundingClientRect();
       const {
         left: tabLeft
-      } = result.getBoundingClientRect();
+      } = selectedItem.value.getBoundingClientRect();
       const left = tabLeft - navLeft;
       indicator.value.style.left = left + "px";
     };
@@ -56,11 +52,6 @@ export default {
         throw new Error("Tabs 子标签必须是 Tab");
       }
     });
-    const current = computed(() => {
-      return defaults.filter((tag) => {
-        return tag.props.title === props.selected;
-      })[0];
-    });
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
@@ -71,7 +62,7 @@ export default {
       defaults,
       titles,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container,
     };
