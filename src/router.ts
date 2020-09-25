f792a3ee1cf28455cfb5e67f7144b73dfd5a2f5f
XiaoPan <2489964425@qq.com>
2020-09-25 13:52:02 +0800

    代码优化

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,14 @@
 import { createWebHashHistory, createRouter } from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
-import Intro from './views/Intro.vue'
-import GetStart from './views/GetStart.vue'
-import Install from './views/Install.vue'
+import Markdown from './components/Markdown.vue'
 
 import DocDemo from './components/DocDemo.vue'
 import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
+import { h } from 'vue'
 
 const history = createWebHashHistory()
 export const router = createRouter({
@@ -21,9 +20,27 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: '', component: DocDemo },
-        { path: 'intro', component: Intro },
-        { path: 'get-start', component: GetStart },
-        { path: 'install', component: Install },
+        {
+          path: 'intro',
+          component: h(Markdown, {
+            path: '../markdown/intro.md',
+            key: '1',
+          }),
+        },
+        {
+          path: 'get-start',
+          component: h(Markdown, {
+            path: '../markdown/get-started.md',
+            key: '2',
+          }),
+        },
+        {
+          path: 'install',
+          component: h(Markdown, {
+            path: '../markdown/install.md',
+            key: '3',
+          }),
+        },
         { path: 'switch', component: SwitchDemo },
         { path: 'button', component: ButtonDemo },
         { path: 'dialog', component: DialogDemo },
