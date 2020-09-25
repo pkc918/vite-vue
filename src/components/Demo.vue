1ebc9fa04eb91549cddd53ca4cebd9da9a489bb9
XiaoPan <2489964425@qq.com>
2020-09-25 19:53:45 +0800

    优化代码

---
+++ src/components/Demo.vue
@@ -0,0 +1,74 @@
+<template>
+<div class="demo">
+  <h2>支持 disabled</h2>
+  <div class="demo-component">
+    <component :is="Switch2Demo"></component>
+  </div>
+  <div class="demo-actions">
+    <Button>查看代码</Button>
+  </div>
+  <div class="demo-code">
+    <pre class="language-html" v-html="
+          Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')
+        "></pre>
+  </div>
+</div>
+</template>
+
+<script>
+import Button from '../lib/Button.vue'
+import Switch1Demo from './Switch1.demo.vue'
+import Switch2Demo from './Switch2.demo.vue'
+import Prism from 'prismjs'
+import '../../public/css/prism.css'
+export default {
+  props: {
+    component: Object,
+  },
+  components: {
+    Button,
+  },
+  setup() {
+    return {
+      Prism,
+      Switch1Demo,
+      Switch2Demo,
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+$border-color: #d9d9d9;
+
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+
+  >h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+
+  &-component {
+    padding: 16px;
+  }
+
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+
+    >pre {
+      line-height: 1.1;
+      font-family: Consolas, 'Courier New', Courier, monospace;
+      margin: 0;
+    }
+  }
+}
+</style>
--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,90 +1,27 @@
-<template lang="">
+<template>
 <div>
   <h1>Switch 组件示例</h1>
-  <div class="demo">
-    <h2>常规用法</h2>
-    <div class="demo-component">
-      <component :is="Switch1Demo"></component>
-    </div>
-    <div class="demo-actions"> <Button>查看代码</Button> </div>
-    <div class="demo-code">
-      <pre class="language-css" v-html="Prism.highlight(Switch1Demo.__sourceCode,
-  Prism.languages.html, 'html')"></pre>
-    </div>
-  </div>
-  <div class="demo">
-    <h2>支持 disabled</h2>
-    <div class="demo-component">
-      <component :is="Switch2Demo"></component>
-    </div>
-    <div class="demo-actions">
-      <Button>查看代码</Button>
-    </div>
-    <div class="demo-code">
-      <pre class="language-css" v-html="Prism.highlight(Switch2Demo.__sourceCode,
-  Prism.languages.html, 'html')"></pre>
-    </div>
-  </div>
+  <Demo :component="Switch1Demo"></Demo>
+  <Demo :component="Switch2Demo"></Demo>
 </div>
 </template>
 
 <script lang="ts">
-import Switch from '../lib/Switch.vue'
 import Button from '../lib/Button.vue'
+import Demo from './Demo.vue'
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
-import Prism from 'prismjs'
-import '../../public/css/prism.css'
-import {
-  ref
-} from 'vue'
+
 export default {
   components: {
     Button,
+    Demo,
   },
   setup() {
-    const bool = ref(false)
     return {
-      bool,
       Switch1Demo,
       Switch2Demo,
-      Prism,
     }
   },
 }
 </script>
-
-<style lang="scss">
-$border-color: #d9d9d9;
-
-.demo {
-  border: 1px solid $border-color;
-  margin: 16px 0 32px;
-
-  >h2 {
-    font-size: 20px;
-    padding: 8px 16px;
-    border-bottom: 1px solid $border-color;
-  }
-
-  &-component {
-    padding: 16px;
-  }
-
-  &-actions {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-  }
-
-  &-code {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-
-    >pre {
-      line-height: 1.1;
-      font-family: Consolas, 'Courier New', Courier, monospace;
-      margin: 0;
-    }
-  }
-}
-</style>
