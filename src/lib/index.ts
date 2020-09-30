aa6b5e0f609c56cb133ed84bfef0cbcfceef7fd9
XiaoPan <2489964425@qq.com>
2020-09-30 23:29:20 +0800

    创建 lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,7 @@
+import { openDialog } from './openDialog'
+export { default as Switch } from './Switch.vue'
+export { default as Button } from './Button.vue'
+export { default as Tabs } from './Tabs.vue'
+export { default as Tab } from './Tab.vue'
+export { default as Dialog } from './Dialog.vue'
+export { openDialog } from './openDialog'
