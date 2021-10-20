fc6e0d787e9040727d1c19672b88b4ba856d389c
XiaoPan <2489964425@qq.com>
2021-10-20 12:04:46 +0800

    修改项目配置

--- rollup.config.js
+++ rollup.config.js
@@ -3,29 +3,34 @@
 import esbuild from 'rollup-plugin-esbuild'
 import vue from 'rollup-plugin-vue'
 import scss from 'rollup-plugin-scss'
-import dartSass from 'sass'
-import { terser } from 'rollup-plugin-terser'
+import dartSass from 'sass';
+import { terser } from "rollup-plugin-terser"
 
 export default {
   input: 'src/lib/index.ts',
-  output: {
+  output: [{
     globals: {
-      vue: 'Vue',
+      vue: 'Vue'
     },
     name: 'Gulu',
     file: 'dist/lib/gulu.js',
     format: 'umd',
-    plugins: [terser()],
-  },
+    plugins: [terser()]
+  }, {
+    name: 'Gulu',
+    file: 'dist/lib/gulu.esm.js',
+    format: 'es',
+    plugins: [terser()]
+  }],
   plugins: [
     scss({ include: /\.scss$/, sass: dartSass }),
     esbuild({
       include: /\.[jt]s$/,
       minify: process.env.NODE_ENV === 'production',
-      target: 'es2015',
+      target: 'es2015'
     }),
     vue({
       include: /\.vue$/,
-    }),
+    })
   ],
 }
